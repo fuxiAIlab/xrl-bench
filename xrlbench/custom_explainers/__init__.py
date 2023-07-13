7223895e723f813053710fb716f5110770aa30c5
熊宇 <xiongyu1@corp.netease.com>
2023-07-13 17:53:52 +0800

    1

--- xrlbench/custom_explainers/__init__.py
+++ xrlbench/custom_explainers/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from xrlbench.custom_explainers.tarbular_shap import TarbularSHAP
+from xrlbench.custom_explainers.tabular_shap import TabularSHAP
 from xrlbench.custom_explainers.sarfa import SARFA
+from xrlbench.custom_explainers.visualize_saliency import VisualizeSaliency
+from xrlbench.custom_explainers.tabular_lime import TabularLime
+from xrlbench.custom_explainers.deep_shap import DeepSHAP
+from xrlbench.custom_explainers.gradient_shap import GradientSHAP
