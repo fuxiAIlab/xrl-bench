7408e84b88b972dcb252db907144b46a3b5693f1
熊宇 <xiongyu1@corp.netease.com>
2023-07-14 16:44:06 +0800

    [UPDATE]

--- xrlbench/custom_metrics/__init__.py
+++ xrlbench/custom_metrics/__init__.py
@@ -4,3 +4,4 @@ from xrlbench.custom_metrics.fidelity.aim import AIM
 from xrlbench.custom_metrics.fidelity.aum import AUM
 from xrlbench.custom_metrics.fidelity.pgu import PGU
 from xrlbench.custom_metrics.fidelity.pgi import PGI
+from xrlbench.custom_metrics.stability.ris import RIS
