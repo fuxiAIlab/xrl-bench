b1b53a487c87b6deb62c3ae68e6ba5930280264d
熊宇 <xiongyu1@corp.netease.com>
2023-07-14 16:40:23 +0800

    [UPDATE]增加fidelity方法代码可读性

--- xrlbench/custom_metrics/fidelity/aim.py
+++ xrlbench/custom_metrics/fidelity/aim.py
@@ -1,28 +1,74 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pandas as pd
 
 
 class AIM:
-    def __init__(self, dataset, **kwargs):
+    def __init__(self, environment, **kwargs):
         """
-        Accuracy on Important feature Masked by zero padding [AIM]
+        Class for evaluating the Accuracy on Important feature Masked by zero padding [AIM].
+
+        Parameters:
+        -----------
+        environment : object
+            The environment used for evaluating XRL methods.
         """
-        self.dataset = dataset
+        self.environment = environment
 
     def evaluate(self, X, y, feature_weights, k=5):
-        accuracy = []
-        if len(np.array(feature_weights).shape) == 2:
-            weights_ranks = [np.argsort(-feature_weights[i]) for i in range(len(feature_weights))]
-        elif len(np.array(feature_weights).shape) == 3:
-            weights_ranks = [np.argsort(-feature_weights[i, :, int(y[i])]) for i in range(len(feature_weights))]
+        """
+        Evaluate the performance of XRL methods using AIM metric.
+
+        Parameters:
+        -----------
+        X : pandas.DataFrame or numpy.ndarray
+            The input data.
+        y : pandas.Series or numpy.ndarray
+            The true labels for the input data.
+        feature_weights : numpy.ndarray
+            The feature weights computed using an XRL method.
+        k : int, optional (default=5)
+            The number of top feature to mask.
+
+        Returns:
+        --------
+        accuracy : float
+            The mean accuracy on important feature masked by zero padding.
+        """
+        # Check inputs
+        if not isinstance(X, (pd.DataFrame, np.ndarray)):
+            raise TypeError("X must be a pandas.DataFrame or a numpy.ndarray")
+        if not isinstance(y, (np.ndarray, pd.Series)):
+            raise TypeError("y must be a numpy.ndarray or pandas.Series")
+        X = X.values if isinstance(X, pd.DataFrame) else X
+        y = y.values if isinstance(y, pd.Series) else y
+        if len(np.array(feature_weights).shape) == 3:
+            feature_weights = [feature_weights[i, :, int(y)] for i in range(len(feature_weights))]
+        elif len(np.array(feature_weights).shape) != 2:
+            raise ValueError("Invalid shape for feature_weights.")
+        weights_ranks = [np.argsort(-feature_weights[i]) for i in range(len(feature_weights))]
+        masked_X = X.copy()
         for i in range(X.shape[0]):
-            if i % 1000 == 0:
-                print(i)
-            X[i][weights_ranks[:k]] = 0
-            action = self.dataset.agent.act(X[i])
-            if action == y[i]:
-                accuracy.append(1)
-            else:
-                accuracy.append(0)
-        return np.mean(accuracy)
+            masked_X[i][weights_ranks[i][:k]] = 0
+        y_pred = self.environment.agent.act(masked_X)
+        accuracy = np.mean(y_pred == y)
+        return accuracy
+
+        # accuracy = []
+        # if len(np.array(feature_weights).shape) == 3:
+        #     feature_weights = [feature_weights[i, :, int(y[i])] for i in range(len(feature_weights))]
+        # weights_ranks = [np.argsort(-feature_weights[i]) for i in range(len(feature_weights))]
+        # # if len(np.array(feature_weights).shape) == 2:
+        # #     weights_ranks = [np.argsort(-feature_weights[i]) for i in range(len(feature_weights))]
+        # # elif len(np.array(feature_weights).shape) == 3:
+        # #     weights_ranks = [np.argsort(-feature_weights[i, :, int(y[i])]) for i in range(len(feature_weights))]
+        # for i in range(X.shape[0]):
+        #     X[i][weights_ranks[:k]] = 0
+        #     action = self.dataset.agent.act(X[i])
+        #     if action == y[i]:
+        #         accuracy.append(1)
+        #     else:
+        #         accuracy.append(0)
+        # return np.mean(accuracy)
+
--- xrlbench/custom_metrics/fidelity/pgu.py
+++ xrlbench/custom_metrics/fidelity/pgu.py
@@ -1,30 +1,82 @@
 # -*- coding: utf-8 -*-
 
-import copy
 import numpy as np
+import pandas as pd
 from xrlbench.utils.perturbation import get_normal_perturbed_inputs
 
 
 class PGU:
-    def __init__(self, dataset, **kwargs):
+    def __init__(self, environment, **kwargs):
         """
-        Prediction Gap on Unimportant feature pertubation [PGU]
+        Class for evaluating the Prediction Gap on Unimportant feature perturbation [PGU].
+
+        Parameters:
+        -----------
+        environment : object
+            The environment used for evaluating XRL methods.
         """
-        self.dataset = dataset
+        self.environment = environment
 
     def evaluate(self, X, y, feature_weights, k=5):
+        """
+        Evaluate the performance of XRL methods using PGI metric.
+
+        Parameters:
+        -----------
+        X : pandas.DataFrame
+            The input data.
+        y : pandas.Series or numpy.ndarray
+            The true labels for the input data.
+        feature_weights : numpy.ndarray
+            The feature weights computed using an XRL method.
+        k : int, optional (default=5)
+            The number of top feature to mask.
+
+        Returns:
+        --------
+        mean prediction gap : float
+            The prediction gap on important feature perturbation.
+        """
+        # Check inputs
+        if not isinstance(X, pd.DataFrame):
+            raise TypeError("X must be a pandas.DataFrame")
+        if not isinstance(y, (np.ndarray, pd.Series)):
+            raise TypeError("y must be a numpy.ndarray or pandas.Series")
+        feature_names = list(X.columns)
+        X = X.values
+        y = y.values if isinstance(y, pd.Series) else y
+        if len(np.array(feature_weights).shape) == 3:
+            feature_weights = [feature_weights[i, :, int(y)] for i in range(len(feature_weights))]
+        elif len(np.array(feature_weights).shape) != 2:
+            raise ValueError("Invalid shape for feature weights.")
         prediction_gap = []
-        if len(np.array(feature_weights).shape) == 2:
-            weights_ranks = [np.argsort(feature_weights[i]) for i in range(len(feature_weights))]
-        elif len(np.array(feature_weights).shape) == 3:
-            weights_ranks = [np.argsort(feature_weights[i, :, int(y[i])]) for i in range(len(feature_weights))]
+        weights_ranks = [np.argsort(feature_weights[i]) for i in range(len(feature_weights))]
+        X_perturbed = X.copy()
+        categorical_feature_inds = [feature_names.index(name) for name in self.environment.categorical_states]
+        X_perturbed = get_normal_perturbed_inputs(X_perturbed, weights_ranks[:, :k], categorical_feature_inds)
         for i in range(X.shape[0]):
-            if i % 1000 == 0:
-                print(i)
-            action_value = self.dataset.agent.inference(X[i])[y]
-            state_backen = copy.deepcopy(X[i])
-            state_perturbed = get_normal_perturbed_inputs(state_backen, weights_ranks[:-k], X)
-            action_values_perturbed = self.dataset.agent.inference(state_perturbed)[y]
-            prediction_gap.append(np.abs(action_value - action_values_perturbed))
+            y_pred = self.environment.agent.inference(X[i])[0][int(y[i])]
+            perturbed_y_pred = self.environment.agent.inference(X_perturbed[i])[0][int(y[i])]
+            prediction_gap.append(np.abs(y_pred - perturbed_y_pred))
         return np.mean(prediction_gap)
+
+
+        # prediction_gap = []
+        # state_names = list(X.columns)
+        # X = X.values
+        # if len(np.array(feature_weights).shape) == 3:
+        #     feature_weights = [feature_weights[i, :, int(y[i])] for i in range(len(feature_weights))]
+        # weights_ranks = [np.argsort(feature_weights[i]) for i in range(len(feature_weights))]
+        # # if len(np.array(feature_weights).shape) == 2:
+        # #     weights_ranks = [np.argsort(feature_weights[i]) for i in range(len(feature_weights))]
+        # # elif len(np.array(feature_weights).shape) == 3:
+        # #     weights_ranks = [np.argsort(feature_weights[i, :, int(y[i])]) for i in range(len(feature_weights))]
+        # states_backen = copy.deepcopy(X)
+        # categorical_state_inds = [state_names.index(name) for name in self.dataset.categorical_states]
+        # states_perturbed = get_normal_perturbed_inputs(states_backen, weights_ranks[:-k], categorical_state_inds)
+        # for i in range(X.shape[0]):
+        #     action_value = self.dataset.agent.inference(X[i])[0][int(y[i])]
+        #     action_values_perturbed = self.dataset.agent.inference(states_perturbed[i])[0][int(y[i])]
+        #     prediction_gap.append(np.abs(action_value - action_values_perturbed))
+        # return np.mean(prediction_gap)
 
