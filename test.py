85de24ceb6d8aefd1e256e579088056ae8f20152
a598872420 <598872420@qq.com>
2023-08-08 15:54:40 +0800

    [UPDATE]更新测试脚本

--- test.py
+++ test.py
@@ -10,64 +10,80 @@
 
 from xrlbench.explainers import Explainer
 from xrlbench.evaluator import Evaluator
-from xrlbench.datasets import DataSet
+from xrlbench.environments import Environment
 import pandas as pd
 
 
 if __name__ == "__main__":
-    # SARFA explain in LunarLander environment
-    dataset = DataSet(data_name="lunarLander")
-    df = dataset.get_dataset(generate=False)
-    df_sample = df.sample(n=5000, random_state=42)
-    action_sample = list(df_sample['action'])
-    state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    explainer = Explainer(method='sarfa', state=state_sample, action=action_sample, model=dataset.agent.qnetwork_local)
-    saliency = explainer.explain()
-    print(saliency)
-    evaluator = Evaluator(metric="AUM", dataset=dataset)
-    aum = evaluator.evaluate(state_sample.values, action_sample, saliency, k=5)
-    print("accuracy:", aum)
+    # environment = Environment(environment_name="cartPole")
+    # environment.train_model()
+    # df = environment.get_dataset(generate=True)
+    # print(df.info())
 
     # TarbularSHAP explain in LunarLander environment
-    # dataset = DataSet(data_name="lunarLander")
-    # df = dataset.get_dataset(generate=False)
-    # action = list(df['action'])
-    # state = df.drop(['action', 'reward'], axis=1)
-    # explainer = Explainer(method="tarbularShap", state=state, action=action)
+    environment = Environment(environment_name="cartPole")
+    # environment.train_model()
+    df = environment.get_dataset(generate=False)
+    df_sample = df.sample(n=5000, random_state=42)
+    action_sample = df_sample['action']
+    print(action_sample)
+    state_sample = df_sample.drop(['action', 'reward'], axis=1)
+    explainer = Explainer(method='tabularShap', state=state_sample, action=action_sample)
+    saliency = explainer.explain()
+    evaluator = Evaluator(metric="AUM", environment=environment)
+    aum = evaluator.evaluate(state_sample, action_sample, saliency, k=2)
+    print("accuracy:", aum)
+
+    # SARFA explain in LunarLander environment
+    # environment = Environment(environment_name="lunarLander")
+    # df = environment.get_dataset(generate=False)
     # df_sample = df.sample(n=5000, random_state=42)
-    # action_sample = list(df_sample['action'])
+    # action_sample = df_sample['action']
     # state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    # shap_values = explainer.explain(state_sample)
-    # print(shap_values.values)
-    # evaluator = Evaluator(metric="PGI", dataset=dataset)
-    # aum = evaluator.evaluate(state_sample, action_sample, shap_values.values, k=5)
+    # explainer = Explainer(method='sarfa', state=state_sample, action=action_sample, model=environment.model)
+    # saliency = explainer.explain()
+    # print(saliency)
+    # evaluator = Evaluator(metric="RIS", environment=environment)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, explainer=explainer)
     # print("accuracy:", aum)
 
-
-
-    # LL = LunarLander(load_model=True)
-    # df = LL.get_dataset()
-    # action = list(df['action'])
-    # state = df.drop(['action', 'reward'], axis=1)
-    # explainer = TarbularSHAP(X=state, y=action)
-    # print(explainer.report)
-    # df_sample = df.sample(n=50000, random_state=42)
-    # action_sample = list(df_sample['action'])
+    # VisualizeSaliency explain in LunarLander environment
+    # environment = Environment(environment_name="lunarLander")
+    # df = environment.get_dataset(generate=False)
+    # df_sample = df.sample(n=5000, random_state=42)
+    # action_sample = df_sample['action']
     # state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    # shap_values = explainer.explain(state_sample)
-    # metric = Fidelity(dataset=LL)
-    # fidelity = metric.evaluate(X=state_sample, y=action_sample, feature_weights=shap_values.values)
-    # print("fidelity:", fidelity)
-
-    # LL = LunarLander(load_model=True)
-    # df = LL.get_dataset()
-    # df_sample = df.sample(n=50000, random_state=42)
-    # action_sample = list(df_sample['action'])
-    # state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    #
-    # explainer = SARFA(X=state_sample, y=action_sample, model=LL.agent.qnetwork_local)
+    # explainer = Explainer(method='visualizeSaliency', state=state_sample, action=action_sample, model=environment.model)
     # saliency = explainer.explain()
-    #
-    # metric = Fidelity(dataset=LL)
-    # fidelity = metric.evaluate(X=state_sample.values, y=action_sample, feature_weights=saliency)
-    # print("fidelity:", fidelity)
+    # print(saliency)
+    # evaluator = Evaluator(metric="AUM", environment=environment)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, k=1)
+    # print("accuracy:", aum)
+
+    # TabularLime explain in LunarLander environment
+    # environment = Environment(environment_name="lunarLander")
+    # df = environment.get_dataset(generate=False)
+    # df_sample = df.sample(n=5000, random_state=42)
+    # action_sample = df_sample['action']
+    # state_sample = df_sample.drop(['action', 'reward'], axis=1)
+    # explainer = Explainer(method='tabularLime', state=state_sample, action=action_sample,
+    #                       model=environment.model)
+    # saliency = explainer.explain()
+    # print(saliency)
+    # evaluator = Evaluator(metric="AUM", environment=environment)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, k=1)
+    # print("accuracy:", aum)
+
+    # GradientSHAP explain in LunarLander environment
+    # environment = Environment(environment_name="lunarLander")
+    # df = environment.get_dataset(generate=False)
+    # df_sample = df.sample(n=5000, random_state=42)
+    # action_sample = df_sample['action']
+    # state_sample = df_sample.drop(['action', 'reward'], axis=1)
+    # explainer = Explainer(method='gradientShap', state=state_sample, action=action_sample,
+    #                       model=environment.model)
+    # saliency = explainer.explain()
+    # print(saliency)
+    # evaluator = Evaluator(metric="AUM", environment=environment)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, k=1)
+    # print("accuracy:", aum)
