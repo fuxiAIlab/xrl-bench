cc206d49e6498052d291c10f6805bfd446cec015
a598872420 <598872420@qq.com>
2023-08-08 16:25:42 +0800

    [UPDATE]更新flappybird导入

--- xrlbench/custom_environment/__init__.py
+++ xrlbench/custom_environment/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from xrlbench.custom_environment.lunarlander.lunar_lander import LunarLander
+from xrlbench.custom_environment.cartpole.cart_pole import CartPole
+from xrlbench.custom_environment.flappybird.flappy_bird import FlappyBird
