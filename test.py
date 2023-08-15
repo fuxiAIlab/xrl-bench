6b9315f7227b8558b2862e3ba4099ae2d9332d17
a598872420 <598872420@qq.com>
2023-08-15 13:48:59 +0800

    [ADD]增加IG解释器测试

--- test.py
+++ test.py
@@ -21,17 +21,17 @@ if __name__ == "__main__":
     # print(df.info())
 
     # TarbularSHAP explain in LunarLander environment
-    environment = Environment(environment_name="lunarLander")
-    # environment.train_model()
-    df = environment.get_dataset(generate=False)
-    df_sample = df.sample(n=5000, random_state=42)
-    action_sample = df_sample['action']
-    state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    explainer = Explainer(method='tabularShap', state=state_sample, action=action_sample)
-    saliency = explainer.explain()
-    evaluator = Evaluator(metric="AUM", environment=environment)
-    aum = evaluator.evaluate(state_sample, action_sample, saliency, k=4)
-    print("accuracy:", aum)
+    # environment = Environment(environment_name="lunarLander")
+    # # environment.train_model()
+    # df = environment.get_dataset(generate=False)
+    # df_sample = df.sample(n=5000, random_state=42)
+    # action_sample = df_sample['action']
+    # state_sample = df_sample.drop(['action', 'reward'], axis=1)
+    # explainer = Explainer(method='tabularShap', state=state_sample, action=action_sample)
+    # saliency = explainer.explain()
+    # evaluator = Evaluator(metric="AUM", environment=environment)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, k=4)
+    # print("accuracy:", aum)
 
     # SARFA explain in LunarLander environment
     # environment = Environment(environment_name="lunarLander")
@@ -39,11 +39,11 @@ if __name__ == "__main__":
     # df_sample = df.sample(n=5000, random_state=42)
     # action_sample = df_sample['action']
     # state_sample = df_sample.drop(['action', 'reward'], axis=1)
-    # explainer = Explainer(method='sarfa', state=state_sample, action=action_sample, model=environment.model)
+    # explainer = Explainer(method='tabularShap', state=state_sample, action=action_sample) # , model=environment.model
     # saliency = explainer.explain()
     # print(saliency)
     # evaluator = Evaluator(metric="RIS", environment=environment)
-    # aum = evaluator.evaluate(state_sample, action_sample, saliency, explainer=explainer)
+    # aum = evaluator.evaluate(state_sample, action_sample, saliency, explainer=explainer) #
     # print("accuracy:", aum)
 
     # VisualizeSaliency explain in LunarLander environment
@@ -86,3 +86,16 @@ if __name__ == "__main__":
     # evaluator = Evaluator(metric="AUM", environment=environment)
     # aum = evaluator.evaluate(state_sample, action_sample, saliency, k=1)
     # print("accuracy:", aum)
+
+    # IntegratedGradient explain in LunarLander environment
+    environment = Environment(environment_name="lunarLander")
+    df = environment.get_dataset(generate=False)
+    df_sample = df.sample(n=5000, random_state=42)
+    action_sample = df_sample['action']
+    state_sample = df_sample.drop(['action', 'reward'], axis=1)
+    explainer = Explainer(method='integratedGradient', state=state_sample, action=action_sample, model=environment.model)
+    saliency = explainer.explain()
+    print(saliency)
+    evaluator = Evaluator(metric="AUM", environment=environment)
+    aum = evaluator.evaluate(state_sample, action_sample, saliency)  # , explainer=explainer
+    print("accuracy:", aum)
