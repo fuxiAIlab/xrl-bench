f7f381ff44af3fc9bbb31add56ff9cb1a78ee147
a598872420 <598872420@qq.com>
2023-10-16 15:38:04 +0800

    [UPDATE]init中增加图像输入解释器

--- xrlbench/custom_metrics/__init__.py
+++ xrlbench/custom_metrics/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from xrlbench.custom_metrics.fidelity.aim import AIM
-from xrlbench.custom_metrics.fidelity.aum import AUM
-from xrlbench.custom_metrics.fidelity.pgu import PGU
-from xrlbench.custom_metrics.fidelity.pgi import PGI
-from xrlbench.custom_metrics.stability.ris import RIS
+from xrlbench.custom_metrics.fidelity.aim import AIM, ImageAIM
+from xrlbench.custom_metrics.fidelity.aum import AUM, ImageAUM
+from xrlbench.custom_metrics.fidelity.pgu import PGU, ImagePGU
+from xrlbench.custom_metrics.fidelity.pgi import PGI, ImagePGI
+from xrlbench.custom_metrics.stability.ris import RIS, ImageRIS
