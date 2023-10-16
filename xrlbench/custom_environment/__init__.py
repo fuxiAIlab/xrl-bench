127c7803946cdad94294f45b25030a09eb4394c4
a598872420 <598872420@qq.com>
2023-10-16 15:25:47 +0800

    [UPDATE]init中更新breakout和pong环境

--- xrlbench/custom_environment/__init__.py
+++ xrlbench/custom_environment/__init__.py
@@ -3,3 +3,5 @@
 from xrlbench.custom_environment.lunarlander.lunar_lander import LunarLander
 from xrlbench.custom_environment.cartpole.cart_pole import CartPole
 from xrlbench.custom_environment.flappybird.flappy_bird import FlappyBird
+from xrlbench.custom_environment.breakout.break_out import BreakOut
+from xrlbench.custom_environment.pong.pong import Pong
