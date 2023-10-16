a25df5efd4146ffa73dd3d4b5d9a850e1e5ee44e
a598872420 <598872420@qq.com>
2023-10-16 15:31:31 +0800

    [UPDATE]init中更新图像输入解释器

--- xrlbench/custom_explainers/__init__.py
+++ xrlbench/custom_explainers/__init__.py
@@ -2,8 +2,9 @@
 
 from xrlbench.custom_explainers.tabular_shap import TabularSHAP
 from xrlbench.custom_explainers.sarfa import SARFA
-from xrlbench.custom_explainers.visualize_saliency import VisualizeSaliency
+from xrlbench.custom_explainers.sarfa import ImageSARFA
+from xrlbench.custom_explainers.perturbation_saliency import PerturbationSaliency, ImagePerturbationSaliency
 from xrlbench.custom_explainers.tabular_lime import TabularLime
-from xrlbench.custom_explainers.deep_shap import DeepSHAP
-from xrlbench.custom_explainers.gradient_shap import GradientSHAP
-from xrlbench.custom_explainers.integrated_gradient import IntegratedGradient
+from xrlbench.custom_explainers.deep_shap import DeepSHAP, ImageDeepSHAP
+from xrlbench.custom_explainers.gradient_shap import GradientSHAP, ImageGradientSHAP
+from xrlbench.custom_explainers.integrated_gradient import IntegratedGradient, ImageIntegratedGradient
