d95f671d6d4dba66389c43399fc301e8f24f0306
a598872420 <598872420@qq.com>
2024-01-18 15:39:23 +0800

    [ADD]增加L33环境

--- xrlbench/custom_environment/__init__.py
+++ xrlbench/custom_environment/__init__.py
@@ -3,5 +3,6 @@
 from xrlbench.custom_environment.lunarlander.lunar_lander import LunarLander
 from xrlbench.custom_environment.cartpole.cart_pole import CartPole
 from xrlbench.custom_environment.flappybird.flappy_bird import FlappyBird
+from xrlbench.custom_environment.dunkcitydynasty.dunk_city_dynasty import DunkCityDynasty
 from xrlbench.custom_environment.breakout.break_out import BreakOut
 from xrlbench.custom_environment.pong.pong import Pong
